129a0530925abc18587e097d2bd2009a738c3d6c
mangelpt <mg20101994@gmail.com>
2021-11-03 16:53:05 -0500

    add icons and footer

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { css, Global } from "@emotion/react";
-
+import { Footer } from "./components/Footer";
 const cssGlobal = css`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap'
 
@@ -11,10 +11,12 @@ const cssGlobal = css`
 
 function App() {
   return (
+
     <main>
-      <Global 
+      <Global
         styles={cssGlobal}
       />
+      <Footer />
     </main>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,38 @@
+import styled from '@emotion/styled'
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { ButtonWhite } from './UI/Buttons'
+import { ArrowIconRight, HistoryIcon, HomeIcon, UserIcon } from './UI/Icons'
+
+
+const StyledFooter = styled.footer`
+
+width: 75vw;
+margin: 10px auto;
+height: fit-content;
+display: flex;
+justify-content:space-between;
+svg{
+  width: 30px;
+  color: gray;
+}
+`;
+
+export const Footer = () => {
+  return (
+    <>
+      <ButtonWhite> History <ArrowIconRight /> </ButtonWhite>
+      <StyledFooter>
+        <NavLink to="/home">
+          <HomeIcon />
+        </NavLink>
+        <NavLink to="/user">
+          <UserIcon />
+        </NavLink>
+        <NavLink to="/history">
+          <HistoryIcon />
+        </NavLink>
+      </StyledFooter>
+    </>
+  )
+}
