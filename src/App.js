4661e1a56167eeb3e400a59d0afcf5f998ad769f
alxdrian <alex_97piscis@outlook.com>
2021-11-03 16:01:10 -0500

    fix css in foodcard component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
 import { css, Global } from "@emotion/react";
+import { FoodCard } from "./components/UI/FoodCard";
 
 const cssGlobal = css`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap'
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
 
   * {
     font-family: 'Source Sans Pro', sans-serif;
+    margin: 0;
+  }
+  
+  body {
+    background-color: #F6F6F9;
   }
 `;
 
