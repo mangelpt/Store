fd02cfb98ce0cf683e3ff19ddaaf67c6e2d9645e
Ana <79344219+Anitazc13@users.noreply.github.com>
2021-11-03 15:03:39 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Input } from "./components/UI/Input";
 
 const cssGlobal = css`
   
-  @import url('https://fonts.googleapis.com/css2?family=Inter&family=Lato&family=Source+Code+Pro&display=swap');
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap');
   * {
     font-family: 'Source Sans Pro', sans-serif;
