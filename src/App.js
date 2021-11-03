1cf165db25854c51d333d1f6ee5a05510da200a5
alxdrian <alex_97piscis@outlook.com>
2021-11-03 12:02:18 -0500

    add firt global styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import React from "react";
+import { css, Global } from "@emotion/react";
 import "./App.css";
 
+const cssGlobal = css`
+  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap'
+
+  * {
+    font-family: 'Source Sans Pro', sans-serif;
+  }
+`;
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Global 
+      styles={cssGlobal}
+    />
+  );
 }
 
 export default App;
