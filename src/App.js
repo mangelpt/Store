55ca7ce0aae29bc51df429ac69c29bd257193c5c
eder <edra1791@gmail.com>
2021-11-03 14:55:45 -0500

    add counter component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { css, Global } from "@emotion/react";
+import { Counter } from "./components/UI/Counter";
 
 const cssGlobal = css`
-  @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap'
+  /* @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap';
 
   * {
     font-family: 'Source Sans Pro', sans-serif;
-  }
+  } */
 `;
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Global 
         styles={cssGlobal}
       />
+      <Counter />
     </main>
   );
 }
