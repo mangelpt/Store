acd9bfd77b54974151cee0e2ac81f5d9f212125d
alxdrian <alex_97piscis@outlook.com>
2021-11-03 23:03:50 -0500

    add loading page

---
+++ src/pages/Load.js
@@ -0,0 +1,21 @@
+import { Loading } from "../components/UI/Loading";
+import styled from "@emotion/styled";
+import background from "../assets/images/background.png"
+
+const Background = styled.div`
+  height: 100vh;
+  width: 414px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-image: url(${background});
+`
+
+export default function Load () {
+  return (
+    <Background>
+      <Loading />
+    </Background>
+  )
+}
