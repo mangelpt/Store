ce4119e23ea0cdec6b6da44e01d690b0915c0c8c
alxdrian <alex_97piscis@outlook.com>
2021-11-03 19:15:57 -0500

    add checkout card component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { CartEmpty } from "./components/UI/CartEmpty";
 import { Footer } from "./components/Footer";
 import { Hero } from "./components/UI/Hero";
 import { TotalPrice } from "./components/TotalPrice";
+import { CheckoutCard } from "./components/UI/CheckoutCard";
+
 const cssGlobal = css`
   @import url('https://fonts.googleapis.com/css2?family=Source+Sans+Pro&family=Lato&family=Comic+Neue:wght@400;700&display=swap');
 
@@ -31,6 +33,11 @@ function App() {
       <HistoryEmpty/>
       <CartEmpty/>
       <Footer />
+      <CheckoutCard 
+        name={"Margarita Flores"}
+        address={"Calle de la Paz, 1"}
+        phone={"+34 654 546 789"}
+      />
       <TotalPrice pricetotal={25.76}/>
     </main>
     
