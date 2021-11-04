df0a13313807a6aac4a90b6b324f671cd653d55b
Anitazc13 <anamaria.zc.13@gmail.com>
2021-11-03 21:32:14 -0500

    Delete the styles not necessary

--- src/App.js
+++ src/App.js
@@ -20,52 +20,6 @@ const cssGlobal = css`
   body {
     background-color: #F6F6F9;
   }
-  .iborrainputfile {
-    font-size:16px; 
-    font-weight:normal;
-    font-family: 'Lato';
-  }
-  .inputfile {
-    width: 0.1px;
-    height: 0.1px;
-    opacity: 0;
-    overflow: hidden;
-    position: absolute;
-    z-index: -1;
-  }
-  
-  .inputfile-5 + label {
-      color: #c39f77;
-  }
-
-  .inputfile-5:focus + label,
-  .inputfile-5.has-focus + label,
-  .inputfile-5 + label:hover {
-      color: #9f8465;
-  }
-
-  .inputfile-5 + label figure {
-      width: 100px;
-      height: 100px;
-      border-radius: 50%;
-      background-color: #c39f77;
-      display: block;
-      padding: 20px;
-      margin: 0 auto 10px;
-  }
-
-  .inputfile-5:focus + label figure,
-  .inputfile-5.has-focus + label figure,
-  .inputfile-5 + label:hover figure {
-      background-color: #9f8465;
-  }
-
-  .inputfile-5 + label svg {
-      width: 100%;
-      height: 100%;
-      fill: #fff;
-  }
-
 `;
 
 function App() {
