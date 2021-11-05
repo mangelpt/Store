4bb2530760382b4051578194f7ce64e38e7e1767
alxdrian <alex_97piscis@outlook.com>
2021-11-05 01:47:21 -0500

    add message of empty results

--- src/pages/Results.js
+++ src/pages/Results.js
@@ -6,6 +6,7 @@ import { Redirect, useLocation, Link } from 'react-router-dom';
 import { Footer } from '../components/Footer';
 import { FoodCard } from '../components/UI/FoodCard';
 import { SearchBar } from '../components/UI/SearchBar';
+import { SearchEmpty } from '../components/UI/SearchEmpty';
 
 const StyledDiv = styled.div`
   width: 100vw;
@@ -14,11 +15,19 @@ const StyledDiv = styled.div`
   align-items: center;
   justify-content: space-between;
   gap: 10px;
-  p{
-    font-size: 18px;
+  position: relative;
+
+  p.count{
     font-weight: 600;
-    margin-bottom: 10px;
+    font-size: 28px;
+    line-height: 35px;
+    margin-bottom: 27px;
     text-align: center;
+  }
+
+  div.center {
+    position: absolute;
+    top: 30vh;
   }
 `;
 
@@ -63,21 +72,26 @@ export const Results = () => {
         value={value}
         search={setValue}
       />
-      <p>
-        Found {results.length} results
-      </p>
-      <List>
-        {results?.map((product) => (
-          <Link to={`/foods/${product.id}/description`} key={product.id}>
-            <FoodCard
-              key={product.id}
-              image={product.picture_url}
-              name={product.name}
-              price={product.price} 
-            />
-          </Link>
-        ))}
-      </List>
+      {results.length > 0 ?
+        <>
+          <p className="count">
+          Found {results.length} results
+          </p>
+          <List>
+            {results?.map((product) => (
+              <Link to={`/foods/${product.id}/description`} key={product.id}>
+                <FoodCard
+                  key={product.id}
+                  image={product.picture_url}
+                  name={product.name}
+                  price={product.price} 
+                />
+              </Link>
+            ))}
+          </List>
+        </>
+        : <div className="center"><SearchEmpty /></div>
+      }
       <Footer />
     </StyledDiv>
   )
